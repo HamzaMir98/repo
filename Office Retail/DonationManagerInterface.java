8aa3d4aaaa06b889759b175f847d724a3dfcd140
HamzaMir9 <60587262+HamzaMir9@users.noreply.github.com>
2020-02-23 21:54:43 -0500

    Update DonationManagerInterface.java

--- Office Retail/DonationManagerInterface.java
+++ Office Retail/DonationManagerInterface.java
@@ -4,38 +4,58 @@
  * @author khandan Monshi, revised by Professor Kartchner
  *
  */
-public interface DonationManagerInterface {
+public interface DonationManageInterface {
 	/**
 	 * Stacks a new donation package  in to the container
 	 * @param dPackage Donation package that is stacked to the container
 	 * Return true if the package is stacked, false if the container is full
 	 * @throws ContainerException if container is full
 	 */
-	public boolean   ManagerLoadcontainer(DonationPackage dPackage) throws  ContainerException;
+	public boolean   managerLoadContainer(DonationPackage dPackage) throws  ContainerException;
 	 
 	/**
 	 * adds a new Volunteer to the volunteer line Queue
 	 * @param v A Volunteer object
-	 * @return true if volunteer is queued successfully , false if queue is full
-	 * @throws VolunteerException if the Volunteer Line queue is full
+	 * @return true if volunteer is queued successfully
+	 * @throws VolunteerException("Volunteer Line is full") if the Volunteer Line queue is full
 	 */
-	public  boolean  ManagerQueueVolunteer(Volunteer  v) throws  VolunteerException;
+	public  boolean  managerQueueVolunteer(Volunteer  v) throws  VolunteerException;
 	 
 	/**
 	 * adds a new Recipient to the queue of the Recipient line
 	 * @param rc a Recipient
 	 * @return true if recipient is queued successfully , false if queue is full
-	 * @throws RecipientException if the Recipient line is full
+	 * @throws RecipientException("Recipient Line is full") if the Recipient line is full
 	 */
-	public boolean ManagerQueueRecipient(Recipient r) throws  RecipientException ;
+	public boolean managerQueueRecipient(Recipient r) throws  RecipientException ;
 
 
 	/**
 	 * Simulates donating a DonationPackage from the container stack by the volunteer from the volunteer queue line to the 
 	 * recipients from the recipients queue line. As a result the package is removed from the container, volunteer will be dequeued
-	 * from  and QUEUED BACK to the volunteer line and recipient will be dequeued from the recipient line.
-	 * @returns 1, if there are no volunteer, 2 if there are no recipients, 3 if container is empty, and 0 if the operation is successful
+	 * from  volunteer line and QUEUED BACK to the volunteer line and recipient will be dequeued from the recipient line.
+	 * @throws VolunteerException("Volunteer Queue is empty") if there are no volunteers
+	 * @throws ContainerExcpetion("Contain is empty") if the container is empty
+	 * @throws RecipientException("Recipient Queue is empty") if there are no recipients
 	 * 
 	 */
-	public  int donatePackage();
+	public  int donatePackage() throws VolunteerException, ContainerException, RecipientException;
+	
+	/**
+	 * Returns an array of DonationPackages
+	 * @return an array of Donation Packages
+	 */
+	public DonationPackage[] managerArrayPackage();
+	
+	/**
+	 * Returns an array of Volunteers
+	 * @return an array of Volunteers
+	 */
+	public Volunteer[] managerArrayVolunteer();
+	
+	/**
+	 * Returns an array of Recipients
+	 * @return an array of Recipients
+	 */
+	public Recipient[] managerArrayRecipient();
 }
